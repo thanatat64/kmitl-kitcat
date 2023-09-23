7434aa4e33f4bb84f04f2eda96d564830e9da4da
Korn Rojrattanapanya <kornkubzaza@gmail.com>
2023-09-24 06:48:03 +0700

    change from register to signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="text-center">
         <h1 className="fs-2">Congratulation you now developing Kitcat Project!</h1>
         <p className="fs-4">โอ้วมายก๊อด คอนแกรทซ์!</p>
-        <Link href="Sign Up">Go to Sign Up</Link>
+        <Link href="/signup">Go to Sign Up</Link>
       </div>
     </main>
   )
