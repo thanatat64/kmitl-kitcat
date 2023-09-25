e5bcbedf446eb31392b30ce19f2ad557e133d772
bbbaipooo <baipoo_B@hotmail.com>
2023-09-26 04:49:42 +0700

    nav ver.final

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -1,27 +1,58 @@
 'use client'
 
-import './NavigationBar.css'
+import './NavigationBar.css';
+import React, { useState } from 'react';
+import Link from 'next/link';
 
-import Link from 'next/link'
-
-interface NavigationBarProps {
-}
+interface NavigationBarProps {}
 
 const NavigationBar: React.FC<NavigationBarProps> = ({}) => {
-    return (
-        <div className="navbar">
-            {/* <div className="navbar container flex-shrink-0"> */}
-            <Link href="/" className="title">Kitcat</Link>
-            {/* <ul className="d-flex flex-row gap-4"> */}
-            <ul>
-                <li><Link href="catsitters">พี่เลี้ยงของเรา</Link></li>
-                <li><Link href="services">ค่าบริการ</Link></li>
-                <li><Link href="guide">วิธิการใช้งาน</Link></li>
-                <li><Link href="about">เกี่ยวกับเรา</Link></li>
-                <li><Link href="signin">เข้าสู่ระบบ</Link></li>
-            </ul>
-        </div>
-    )
-}
+  const [activeItem, setActiveItem] = useState<string>('');
 
-export default NavigationBar
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
+
+  const isItemActive = (item: string) => {
+    return item === activeItem ? 'active' : '';
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container flex-shrink-0">
+        <Link href="/" className="title" >
+          Kitcat
+        </Link>
+        <ul>
+          <li>
+            <Link href="/catsitters" className={isItemActive('/catsitters')} onClick={() => handleItemClick('/catsitters')}>
+              พี่เลี้ยงของเรา
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className={isItemActive('/services')} onClick={() => handleItemClick('/services')}>
+              ค่าบริการ
+            </Link>
+          </li>
+          <li>
+            <Link href="/guide" className={isItemActive('/guide')} onClick={() => handleItemClick('/guide')}>
+              วิธิการใช้งาน
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className={isItemActive('/about')} onClick={() => handleItemClick('/about')}>
+              เกี่ยวกับเรา
+            </Link>
+          </li>
+          <li>
+            <Link href="/signin" className={isItemActive('/signin')} onClick={() => handleItemClick('/signin')}>
+              เข้าสู่ระบบ
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
