5450ad6b66fc9e0cbc2fe9e9d2c8a46612b86394
Game Bodinphat <64010451@kmitl.ac.th>
2023-09-25 19:57:59 +0700

    bannerHomePageUpdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,33 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import { KCPUser } from "@/lib/method/KCPUser"
-import { User } from "@/lib/class/User"
-import UsersDisplay from "@/components/displays/UsersDisplay"
+import { useEffect, useState } from "react";
+import { KCPUser } from "@/lib/method/KCPUser";
+import { User } from "@/lib/class/User";
+import UsersDisplay from "@/components/displays/UsersDisplay";
+import Banner from "@/components/displays/banner/Banner";
 
 export default function Page() {
-    const [users, setUsers] = useState<User[]>([])
+    const [users, setUsers] = useState<User[]>([]);
 
     async function fetchUsersData() {
         try {
-            const response = await fetch('/api/user/getall')
-            const usersData = KCPUser.processObjects(await response.json())
-            setUsers(usersData)
+            const response = await fetch("/api/user/getall");
+            const usersData = KCPUser.processObjects(await response.json());
+            setUsers(usersData);
         } catch (error) {
-            console.error('Error fetching users:', error)
+            console.error("Error fetching users:", error);
         }
     }
 
     useEffect(() => {
-        fetchUsersData()
-    }, [])
+        fetchUsersData();
+    }, []);
 
     return (
         <section>
-            <h1>Home</h1>
-            <UsersDisplay users={users} />
+            {/* <h1>Home</h1> */}
+            {/* <UsersDisplay users={users} /> */}
+            <Banner />
         </section>
-    )
+    );
 }
