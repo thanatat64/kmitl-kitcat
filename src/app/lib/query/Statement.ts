3f3c4cdc1f35e07b6b2b9fdc7c7b8cf34aa95a2a
Korn Rojrattanapanya <kornkubzaza@gmail.com>
2023-09-25 22:07:02 +0700

    Clean up

--- src/app/lib/query/Statement.ts
+++ src/app/lib/query/Statement.ts
@@ -1,45 +1,45 @@
-// import { Connection } from '@/lib/database/Connection'
-// import * as sqlite3 from 'sqlite3'
+import { Connection } from '@/lib/database/Connection'
+import * as sqlite3 from 'sqlite3'
 
-// export class Statement {
-//     static getFieldNames(table: string): Promise<string[]> {
-//         const database = Connection.getDatabase()
-//         return new Promise<string[]>((resolve, reject) => {
-//             database.all(`PRAGMA table_info(${table})`, (error, rows: any) => {
-//                 if (error)
-//                     reject(error)
-//                 else {
-//                     const fieldNames = rows.map((row: any) => row.name)
-//                     resolve(fieldNames)
-//                 }
-//             })
-//         })
-//     }
-//     static prepare(query: string, params?: any[]): Promise<sqlite3.Statement> {
-//         const database = Connection.getDatabase()
-//         return new Promise<sqlite3.Statement>((resolve, reject) => {
-//             const statement = database.prepare(query, params, (error: any) => {
-//                 if (error)
-//                     reject(error)
-//                 else
-//                     resolve(statement)
-//             })
-//         })
-//     }
-//     static getEmptyFieldNames(table: string): Promise<{ [fieldName: string]: null }> {
-//         const database = Connection.getDatabase()
-//         return new Promise<{ [fieldName: string]: null }>((resolve, reject) => {
-//             database.all(`PRAGMA table_info(${table})`, (error, rows: any) => {
-//                 if (error)
-//                     reject(error)
-//                 else {
-//                     const emptyFields: { [fieldName: string]: null } = {}
-//                     rows.forEach((row: any) => {
-//                         emptyFields[row.name] = null
-//                     });
-//                     resolve(emptyFields)
-//                 }
-//             })
-//         })
-//     }
-// }
+export class Statement {
+    static getFieldNames(table: string): Promise<string[]> {
+        const database = Connection.getDatabase()
+        return new Promise<string[]>((resolve, reject) => {
+            database.all(`PRAGMA table_info(${table})`, (error, rows: any) => {
+                if (error)
+                    reject(error)
+                else {
+                    const fieldNames = rows.map((row: any) => row.name)
+                    resolve(fieldNames)
+                }
+            })
+        })
+    }
+    static prepare(query: string, params?: any[]): Promise<sqlite3.Statement> {
+        const database = Connection.getDatabase()
+        return new Promise<sqlite3.Statement>((resolve, reject) => {
+            const statement = database.prepare(query, params, (error: any) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(statement)
+            })
+        })
+    }
+    static getEmptyFieldNames(table: string): Promise<{ [fieldName: string]: null }> {
+        const database = Connection.getDatabase()
+        return new Promise<{ [fieldName: string]: null }>((resolve, reject) => {
+            database.all(`PRAGMA table_info(${table})`, (error, rows: any) => {
+                if (error)
+                    reject(error)
+                else {
+                    const emptyFields: { [fieldName: string]: null } = {}
+                    rows.forEach((row: any) => {
+                        emptyFields[row.name] = null
+                    });
+                    resolve(emptyFields)
+                }
+            })
+        })
+    }
+}
