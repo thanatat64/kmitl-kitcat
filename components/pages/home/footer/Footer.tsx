92ee0a809cd038a1020561c66d0384a9bcfdf761
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-09-26 19:27:49 +0700

    addFooter

--- components/pages/home/footer/Footer.tsx
+++ components/pages/home/footer/Footer.tsx
@@ -1,8 +1,34 @@
+'use react'
+
+import { BsLine } from "react-icons/bs"
+import { BsFillTelephoneFill } from "react-icons/bs"
+import { TbMailFilled } from "react-icons/tb"
 import "./Footer.css"
 
 const Footer = () => {
     return ( 
-        <div>asd</div>
+        <div className="app d-flex bg-white p-4">
+            <div className="d-flex justify-content-center w-50 mt-1">
+                <div>
+                    <h5>บริษัท คิทแคท จำกัด</h5>
+                    <h5>เลขที่ 1 ซอยฉลองกรุง 1 ถนนฉลองกรุง แขวงลาดกระบัง</h5>
+                    <h5>เขตลาดกระบัง กรุงเทพฯ 10520</h5>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center w-50 mt-1">
+                <div>
+                    <div className="d-flex">
+                        <h5>ติดต่อเรา:</h5>
+                        <div className="ms-2">
+                            <h5><BsLine /> @KitCat</h5>
+                            <h5><BsFillTelephoneFill /> 02-456-7890</h5>
+                            <h5><TbMailFilled /> KitCat@gmail.com</h5>
+                        </div>
+                    </div>
+                    <h5>Copyright © 2023 KitCat. All Rights Reserved</h5>
+                </div>
+            </div>
+        </div>
      );
 }
  
