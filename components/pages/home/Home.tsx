feb5b3fcafb143797a0a9ff42a1df6f784418f8e
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-09-26 14:35:09 +0700

    addFooter

--- components/pages/home/Home.tsx
+++ components/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import BannerTop from "./banners/BannerTop";
 import Card from "./cards/Card";
+import Footer from "./footer/Footer"
 
 const Home = () => {
     return ( 
         <div>
             <BannerTop />
             <Card />
+            <Footer />
         </div>
      );
 }
