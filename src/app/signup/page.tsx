5a23b314ea601dbe97e6becb314c28059b4138a6
Thanatat Chongkittisakul <thanatat6@gmail.com>
2023-09-27 23:31:11 +0700

    update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     return (
         <section>
             <div>
-                <div className="layoutOfBacktohome">
+                <div className="layoutOfBacktoSignIn">
                     <Link href={"/signin"}>
                         <button type="button" className="myBtn btn btn-light rounded-pill border border-dark">ย้อนกลับ</button>
                     </Link>
