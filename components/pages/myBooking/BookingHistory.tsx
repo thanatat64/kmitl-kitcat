71324d9f23c7290e7b8ac82601bdd15cbb0f68b3
Game Bodinphat <64010451@kmitl.ac.th>
2023-10-11 04:30:59 +0700

    UpdateMybooking

--- components/pages/myBooking/BookingHistory.tsx
+++ components/pages/myBooking/BookingHistory.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BookingHistory: React.FC = () => {
     return (
-        <div className="bg-blue-200 p-4 rounded">
+        <div className="h-[32rem] bg-[var(--cream)] p-4 rounded-tr-[50px] rounded-br-[50px] rounded-bl-[50px] shadow-[0_10px_10px_0px_rgba(0,0,0,0.15)]">
             <div className="flex flex-col">
                 <h2 className="text-2xl font-bold mb-4 text-center">Modal 2 Content</h2>
             </div>
--- components/pages/myBooking/MyStatus.tsx
+++ components/pages/myBooking/MyStatus.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Mystatus: React.FC = () => {
     return (
-        <div className="bg-green-200 p-4 rounded">
+        <div className="h-[32rem] bg-[var(--cream)] p-4 rounded-tr-[50px] rounded-br-[50px] rounded-bl-[50px] shadow-[0_10px_10px_0_rgba(0,0,0,0.15)]">
             <div className="flex flex-col">
                 <h2 className="text-2xl font-bold mb-4 text-center">Modal 1 Content</h2>
             </div>
