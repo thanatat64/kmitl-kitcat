9a7579dc9afaef8eb7e199ba0d2d059e4ff016a8
Game Bodinphat <64010451@kmitl.ac.th>
2023-10-11 02:23:29 +0700

    updatePageMybooking

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -7,29 +7,35 @@ import BookingHistory from "@/components/pages/myBooking/BookingHistory";
 const page: React.FC = () => {
     const [showContent1, setShowContent1] = useState(true);
     const [showContent2, setShowContent2] = useState(false);
+    const [button2Color, setButton2Color] = useState("bg-blue-500");
 
     const toggleContent1 = () => {
         setShowContent1(true);
         setShowContent2(false);
+        setButton2Color("bg-blue-500"); // Reset button2Color to blue when toggling Content1
     };
 
     const toggleContent2 = () => {
         setShowContent1(false);
         setShowContent2(true);
+        setButton2Color("bg-red-400"); // Set button2Color to red when toggling Content2
     };
 
     return (
         <div>
-            <div className="min-h-screen ">
+            <div className="min-h-screen">
+                <div className="text-center font-bold text-4xl text-[var(--navy)] mt-9 mb-6">
+                    การจองของฉัน
+                </div>
                 <button
                     onClick={toggleContent1}
-                    className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                    className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-t-lg"
                 >
                     Toggle Content1
                 </button>
                 <button
                     onClick={toggleContent2}
-                    className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                    className={`text-white py-2 px-4 rounded-t-lg ${button2Color}`}
                 >
                     Toggle Content2
                 </button>
