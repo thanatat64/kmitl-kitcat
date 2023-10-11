a3c58cd1323c8dfc485afdc44dac1e712685d9fe
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-10-11 14:56:14 +0700

    footer responsive

--- components/pages/home/footer/Footer.tsx
+++ components/pages/home/footer/Footer.tsx
@@ -10,13 +10,13 @@ const Footer = () => {
             <div className="container ">
                 <div className="flex p-3 flex-col md:flex-row md:justify-between " >
                     <div className="flex flex-col ">
-                        <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px]">บริษัท คิทแคท จำกัด</h5>
-                        <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] md:pr-[50px]">เลขที่ 1 ซอยฉลองกรุง 1 ถนนฉลองกรุง แขวงลาดกระบัง</h5>
-                        <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px]">เขตลาดกระบัง กรุงเทพฯ 10520</h5>
+                        <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] justify-center md:justify-start">บริษัท คิทแคท จำกัด</h5>
+                        <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] justify-center md:justify-start md:pr-[50px]">เลขที่ 1 ซอยฉลองกรุง 1 ถนนฉลองกรุง แขวงลาดกระบัง</h5>
+                        <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] justify-center md:justify-start">เขตลาดกระบัง กรุงเทพฯ 10520</h5>
                     </div>
                     <div className="flex flex-col ">
                         <div className="flex md:flex-row flex-col">
-                            <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] ">ติดต่อเรา:</h5>
+                            <h5 className="text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] text-center md:text-start ">ติดต่อเรา:</h5>
                             <div className=" flex flex-col md:pl-0">
                                 <h5 className="flex text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] justify-center md:justify-start"><BsLine className="ml-2 mr-2"/>@KitCat</h5>
                                 <h5 className="flex text-[var(--navy)] text-[16px] font-medium md:text-[18px] lg:text-[20px] justify-center md:justify-start"><BsFillTelephoneFill className="ml-2 mr-2"/>02-456-7890</h5>
