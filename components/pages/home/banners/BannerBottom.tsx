71d3c17d7e43f3dfcaa42e2ec5bb5301870f5e18
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-10-12 03:03:46 +0700

    bottom banner support responsive

--- components/pages/home/banners/BannerBottom.tsx
+++ components/pages/home/banners/BannerBottom.tsx
@@ -5,31 +5,33 @@ import catBottomBg from "../../../../public/image/cat-bottom-bg.svg";
 
 export default function BannerBottom() {
     return (
-        <div className="bg-[var(--white-cream)]">
-            <section className="container">
-                <div className="flex flex-row items-center">
+        <div className="bg-[var(--white-cream)] w-screen">
+            <section className="container flex justify-center">
+                <div className="flex flex-col md:flex-row items-center lg:w-[1200px] md:w-[600px]">
 
                     <div className="mb-4 flex justify-center">
-                        <Image width={650} src={catBottomBg} alt='catBottomBg' />
+                        <Image className=" w-[320px] md:w-[300px] lg:w-[640px]" src={catBottomBg} alt='catBottomBg' />
                     </div>
 
-                    <div className="flex flex-col mx-auto">
-                        <div className="lg:text-[48px] font-bold text-[var(--navy)] mb-3">
+                    <div className="flex flex-col md:ml-7 lg:ml-[110px]">
+                        <div className="text-[28px] md:text-[30px] lg:text-[48px] font-bold text-[var(--navy)] md:mt-2 lg:mb-3">
                             มาร่วมเป็นพี่เลี้ยงแมว
                             <br />
                             กับ KitCat
                         </div>
-                        <div className="lg:text-[24px] font-medium text-[var(--navy)] mb-3">
+                        <div className="text-[20px] lg:text-[24px] font-medium text-[var(--navy)] lg:mb-3">
                             ผลตอบแทนสูง สมัครได้ง่าย ๆ
                         </div>
-                        <div className="lg:text-[24px] font-medium text-[var(--navy)] mb-4">
+                        <div className="text-[20px] lg:text-[24px] font-medium text-[var(--navy)] mb-3 md:mb-2 lg:mb-4">
                             ไม่เสียค่าใช้จ่าย คลิกเลย!
                         </div>
-                        <a href="#" className="w-[193px]">
-                            <button className="bg-[var(--red)] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                สมัครเป็นพี่เลี้ยง
-                            </button>
-                        </a>
+                        <div className="mb-4">
+                            <a href="#" className="w-[193px]">
+                                <button className="bg-[var(--red)] text-[20px] font-medium hover:bg-blue-700 text-white  py-2 px-4 rounded-full">
+                                    สมัครเป็นพี่เลี้ยง
+                                </button>
+                            </a>
+                        </div>
                     </div>
 
                 </div>
