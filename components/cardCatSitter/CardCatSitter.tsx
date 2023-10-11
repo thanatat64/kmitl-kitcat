4deabcb169615e9bf00633451fa0fcf29ae1945a
bbbaipooo <baipoo_B@hotmail.com>
2023-10-11 16:13:13 +0700

    cardCatSitter responsive

--- components/cardCatSitter/CardCatSitter.tsx
+++ components/cardCatSitter/CardCatSitter.tsx
@@ -40,15 +40,15 @@ const CardCatSitter: React.FC<CardCatSiiterProps> = ({
   };
   return (
     <div>
-      <div className={`w-[330px] h-[420px] item-center bg-white rounded-[20px] ${isButton&&'h-[489px]'}`}>
+      <div className={`w-[297px] md:w-[330px] h-[420px] item-center bg-white rounded-[20px] ${isButton&&'h-[489px]'}`}>
         <div onClick={openModal} >
-          <div className={`w-[330px] h-[137px] ${color} rounded-t-[20px]`} />
-          <div className='w-[140px] h-[140px] rounded-full mt-[-67px] ml-[90px] bg-white flex justify-center items-end'>
+          <div className={`w-[297px] md:w-[330px] h-[137px] ${color} rounded-t-[20px]`} />
+          <div className='w-[126px] md:w-[140px] h-[126px] md:h-[140px] rounded-full mt-[-67px] ml-[90px] bg-white flex justify-center items-end'>
             <Image className="rounded-full" width={120} src={avatar} alt="avatar" />
           </div>
           <div className='w-[274px] h-[171px] bg-white ml-[25px] mt-[25px] text-[20px] font-semibold text-[var(--navy)]'>
             <div className='text-center p-2'>{name}</div>
-            <div className=' p-2 flex justify-between items-center border-b-[1px] border-[var(--navy)] mr-2 ml-2'>
+            <div className=' p-2 flex justify-between items-center border-b-[1px] border-[var(--navy)] mr-[20px] ml-0 md:mr-2 md:ml-2'>
               <div>{rating}</div>
               <div><Image width={68} src={ratingIcon} alt="ratingIcon" /></div>
               <div>{review} รีวิว</div>
@@ -60,7 +60,7 @@ const CardCatSitter: React.FC<CardCatSiiterProps> = ({
         </div>
         {isButton&&(
           <Link href="/booking/conclusion">
-            <button className='bottom-0 mt-[14px] w-[330px] h-[69px] rounded-b-[20px] bg-[var(--aqua)] text-white text-[20px] font-semibold'  >เลือกพี่เลี้ยง</button>
+            <button className='w-[295px] md:w-[330px] bottom-0 mt-[14px] w-[330px] h-[69px] rounded-b-[20px] bg-[var(--aqua)] text-white text-[20px] font-semibold'  >เลือกพี่เลี้ยง</button>
           </Link>
         )}
 
