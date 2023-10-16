85fa1249f2deab460a542b4b2f40779ee0331d4e
Korn Rojrattanapanya <kornkubzaza@gmail.com>
2023-10-16 09:26:16 +0700

    Fix Import Error

--- components/pages/myOrder/OrderObject/WaitForReview.tsx
+++ components/pages/myOrder/OrderObject/WaitForReview.tsx
@@ -1,18 +1,16 @@
 'use client'
 
-import React, { useState } from "react";
-import Modal from "react-modal"
-import Image from 'next/image'
-import fast from '@/image/goFast.png'
+import ReviewDone from "@/components/pages/myOrder/OrderObject/reviewDone";
 import finished from '@/image/finish.png'
 import hourglass from '@/image/hourglass.png'
-import ReviewDone from "./ReviewDone";
+import Image from 'next/image'
+import React from "react";
 
 interface WaitForReviewProps {
     status: number;
 }
 
-const WaitForReview: React.FC<WaitForReviewProps> = ({ status }) => {
+const WaitForReview: React.FC<WaitForReviewProps> = ({status}) => {
     return (
         <div>
             {status === 3 ? (<ReviewDone/>) : status === 4 ?
@@ -23,7 +21,7 @@ const WaitForReview: React.FC<WaitForReviewProps> = ({ status }) => {
                     <div className="flex flex-col items-center w-[12.5rem]">
                         <h3 className="mt-3 font-extrabold">รีวิวเสร็จสิ้น</h3>
                         <div className='working w-[12.5rem] h-[12.5rem] rounded-full bg-[var(--yellow)] border flex justify-center items-center'>
-                            <Image width={117} src={finished} alt="finished" />
+                            <Image width={117} src={finished} alt="finished"/>
                         </div>
                     </div>
                 </div>) :
@@ -34,7 +32,7 @@ const WaitForReview: React.FC<WaitForReviewProps> = ({ status }) => {
                     <div className="flex flex-col items-center w-[12.5rem]">
                         <h3 className="mt-3 font-extrabold">รอการรีวิว</h3>
                         <div className='working w-[12.5rem] h-[12.5rem] rounded-full bg-[var(--grey)] border flex justify-center items-center'>
-                            <Image width={117} src={hourglass} alt="waiting for review" />
+                            <Image width={117} src={hourglass} alt="waiting for review"/>
                         </div>
                     </div>
                 </div>)
