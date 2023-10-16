9d0b5ee4ecb4fd92cf36b1ad4440cd99579a1026
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-10-17 04:33:38 +0700

    update

--- components/navigation/NavigationBar.tsx
+++ components/navigation/NavigationBar.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import Logo from "@/image/kitCatLogo.svg";
-import {IUser} from '@/lib/class/User';
+import { IUser } from '@/lib/class/User';
 import Image from 'next/image';
 import Link from 'next/link';
-import {usePathname, useRouter} from 'next/navigation';
-import React, {useState} from 'react';
-import {useCookies} from 'react-cookie';
-import {AiOutlineClose} from 'react-icons/ai';
-import {HiMenuAlt3} from 'react-icons/hi';
-import {PiUserCircle} from 'react-icons/pi'
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { AiOutlineClose } from 'react-icons/ai';
+import { HiMenuAlt3 } from 'react-icons/hi';
+import { PiUserCircle } from 'react-icons/pi'
 import Swal from 'sweetalert2'
+import OutsideClickHandler from "react-outside-click-handler";
 
 interface NavigationBarProps {
     user: IUser | undefined,
@@ -83,7 +84,7 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ user, setUser }) => {
         { href: '/about', text: 'เกี่ยวกับเรา', hoverColor: 'hover:bg-[linear-gradient(90deg,_var(--light-red)_0%,_white_100%)]' },
         { href: '/mybooking', text: 'การจองของฉัน', hoverColor: 'hover:bg-[linear-gradient(90deg,_var(--pink)_0%,_white_100%)]' },
         { href: '/myorder', text: 'ออเดอร์ของฉัน', hoverColor: 'hover:bg-[linear-gradient(90deg,_var(--light-blue)_0%,_white_100%)]' },
-        
+
     ];
 
     return (
@@ -93,61 +94,64 @@ const NavigationBar: React.FC<NavigationBarProps> = ({ user, setUser }) => {
                     <Image src={Logo} width={135} alt="" />
                 </Link>
                 {showMobileMenu ? (
-                    <div className="block lg:hidden w-full fixed top-0 left-0 bg-white transition-all duration-800ms shadow-xl py-8 px-12 rounded-b-xl">
-                        <AiOutlineClose
-                            size={25}
-                            className="absolute top-5 right-5 cursor-pointer"
-                            onClick={() => setShowMobileMenu(false)}
-                        />
-                        {showMobileMenu && (
-                            <ul className="pt-[60px] items-center flex flex-col gap-8">
-                                <div className='relative inline-block'>
-                                    {!user ?
-                                        <Link href="/signin" className="text-[20px] text-center font-medium text-[var(--navy)] no-underline rounded-full py-[3px] px-4 border-2 border-[var(--navy)]">
-                                            เข้าสู่ระบบ
-                                        </Link> : <div></div>
-                                    }
-                                    {user ?
-                                        <div
-                                            className='flex flex-row rounded-full py-[2px] px-2 border-2 border-[var(--navy)] cursor-pointer'
-                                            onClick={toggleNabbers}
+                    <OutsideClickHandler onOutsideClick={() => setShowMobileMenu(false)} >
+                        <div className="block lg:hidden w-full fixed top-0 left-0 bg-white transition-all duration-800ms shadow-xl py-8 px-12 rounded-b-xl">
+                            <AiOutlineClose
+                                size={25}
+                                className="absolute top-5 right-5 cursor-pointer"
+                                onClick={() => setShowMobileMenu(false)}
+                            />
+                            {showMobileMenu && (
+                                <ul className="pt-[60px] items-center flex flex-col gap-8">
+                                    <div className='relative inline-block'>
+                                        {!user ?
+                                            <Link href="/signin" className="text-[20px] text-center font-medium text-[var(--navy)] no-underline rounded-full py-[3px] px-4 border-2 border-[var(--navy)]">
+                                                เข้าสู่ระบบ
+                                            </Link> : <div></div>
+                                        }
+                                        {user ?
+                                            <div
+                                                className='flex flex-row rounded-full py-[2px] px-2 border-2 border-[var(--navy)] cursor-pointer'
+                                                onClick={toggleNabbers}
+                                            >
+                                                <div className='w-[33px] h-[33px] mr-1'>
+                                                    <PiUserCircle className='text-[var(--navy)] w-full h-full' />
+                                                </div>
+                                                <div className="text-[20px] font-medium text-[var(--navy)] my-auto overflow-hidden whitespace-nowrap truncate max-w-[110px]">
+                                                    {user.name}
+                                                </div>
+                                            </div> : <div></div>
+                                        }
+                                        {showNabbers && (
+                                            <div className='text-center h-[150px] py-2 border-b-2 border-[var(--navy)]'>
+                                                <div className='mt-4 hover:bg-[linear-gradient(90deg,_var(--yellow)_0%,_white_100%)] '>
+                                                    <Link
+                                                        href="/profile"
+                                                        className="text-[var(--navy)] cursor-pointer w-full text-[20px] font-medium no-underline "
+                                                    >
+                                                        การตั้งค่า
+                                                    </Link>
+                                                </div>
+                                                <p className='py-1 hover:bg-[linear-gradient(90deg,_var(--red)_0%,_white_100%)] cursor-pointer text-[20px] text-[var(--navy)] font-medium mt-3'>ออกจากระบบ</p>
+                                            </div>
+                                        )}
+                                    </div>
+                                    {mobileMenuLinks.map((menuLink, i) => (
+                                        <Link
+                                            key={i}
+                                            href={menuLink.href}
+                                            className={`text-[var(--navy)] mx-auto cursor-pointer w-full text-center text-[20px] font-medium no-underline ${menuLink.hoverColor}`}
+                                            onMouseEnter={() => handleMouseEnter(menuLink.href)}
+                                            onMouseLeave={handleMouseLeave}
+                                            onClick={() => setShowMobileMenu(false)}
                                         >
-                                            <div className='w-[33px] h-[33px] mr-1'>
-                                                <PiUserCircle className='text-[var(--navy)] w-full h-full' />
-                                            </div>
-                                            <div className="text-[20px] font-medium text-[var(--navy)] my-auto overflow-hidden whitespace-nowrap truncate max-w-[110px]">
-                                                {user.name}
-                                            </div>
-                                        </div> : <div></div>
-                                    }
-                                    {showNabbers && (
-                                        <div className='text-center h-[150px] py-2 border-b-2 border-[var(--navy)]'>
-                                            <div className='mt-4 hover:bg-[linear-gradient(90deg,_var(--yellow)_0%,_white_100%)] '>
-                                                <Link
-                                                    href="/profile"
-                                                    className="text-[var(--navy)] cursor-pointer w-full text-[20px] font-medium no-underline "
-                                                >
-                                                    การตั้งค่า
-                                                </Link>
-                                            </div>
-                                            <p className='py-1 hover:bg-[linear-gradient(90deg,_var(--red)_0%,_white_100%)] cursor-pointer text-[20px] text-[var(--navy)] font-medium mt-3'>ออกจากระบบ</p>
-                                        </div>
-                                    )}
-                                </div>
-                                {mobileMenuLinks.map((menuLink, i) => (
-                                    <Link
-                                        key={i}
-                                        href={menuLink.href}
-                                        className={`text-[var(--navy)] mx-auto cursor-pointer w-full text-center text-[20px] font-medium no-underline ${menuLink.hoverColor}`}
-                                        onMouseEnter={() => handleMouseEnter(menuLink.href)}
-                                        onMouseLeave={handleMouseLeave}
-                                    >
-                                        {menuLink.text}
-                                    </Link>
-                                ))}
-                            </ul>
-                        )}
-                    </div>
+                                            {menuLink.text}
+                                        </Link>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                    </OutsideClickHandler>
                 ) : (
                     <ul className="hidden lg:flex items-center gap-2">
                         {mobileMenuLinks.map((menuLink, i) => (
