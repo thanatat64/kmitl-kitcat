0f6025c3ab0246a44e27a744726e48e183e9e3c7
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-10-17 04:05:10 +0700

    support responsive

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,16 +2,14 @@
 
 import "@/components/pages/auth/AuthForm.css"
 import SignInForm from "@/components/pages/auth/signin/SignInForm"
-import Link from "next/link"
 import {useAppContext} from "../context/app"
 
 export default function Page() {
     const {setUser} = useAppContext()
 
     return (
-        <section>
-            <Link href={"/"} className="back-button">กลับสู่หน้าหลัก</Link>
+        <div className="className='auth-section'">
             <SignInForm setUser={setUser}/>
-        </section>
+        </div>
     )
 }
