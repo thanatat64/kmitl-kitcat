8e1e7bd752f051eb500a71bf24b7f024312521b6
Chanidapa Wongtep <ddreamc1@gmail.com>
2023-10-17 17:04:32 +0700

    update

--- src/app/catsitters/page.tsx
+++ src/app/catsitters/page.tsx
@@ -21,14 +21,12 @@ export default function Page() {
             <div className="container flex-shrink-0">
                 <h1 className="text-center pt-5 mb-5 text-[40px] font-bold text-[var(--navy)]">พี่เลี้ยงของเรา</h1>
                 <div className="flex flex-wrap gap-x-12 gap-y-10 justify-center pb-[100px]">
-<<<<<<< HEAD
                     <CardCatSitter name="สมศรี รักสะอาด" rating={4.0} heart={86} review={7} detail="lorem" color="bg-[var(--blue)]" />
                     <CardCatSitter name="สมศรี รักสะอาด" rating={4.0} heart={86} review={7} detail="lorem" color="bg-[var(--yellow)]" />
                     <CardCatSitter name="สมศรี รักสะอาด" rating={4.0} heart={86} review={7} detail="lorem" color="bg-[var(--light-red)]"/>
                     <CardCatSitter name="สมศรี รักสะอาด" rating={4.0} heart={86} review={7} detail="lorem" color="bg-[var(--blue)]" />
                     <CardCatSitter name="สมศรี รักสะอาด" rating={4.0} heart={86} review={7} detail="lorem" color="bg-[var(--yellow)]"/>
                     <CardCatSitter name="สมศรี รักสะอาด" rating={4.0} heart={86} review={7} detail="lorem" color="bg-[var(--light-red)]" />
-=======
                     {catsitters.length > 0 ? (
                         <ul>
                             {catsitters.map((catsitter: IUser) => catsitter ? (
@@ -36,7 +34,6 @@ export default function Page() {
                             ) : <li></li>)}
                         </ul>
                     ) : catsitters.length == 0 ? (<p>บ๋อแบ๋~</p>) : (<p>Loading...</p>)}
->>>>>>> a85343415d3a65b14242f4f983d87edbc3331a76
                 </div>
             </div>
         </div>
